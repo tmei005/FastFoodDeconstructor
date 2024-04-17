a496fe949fc09558427c60375c349cd9fd904d0b
tmei005 <tmei@ufl.edu>
2024-04-17 12:06:42 -0400

    m

--- backend/main.py
+++ backend/main.py
@@ -7,12 +7,16 @@ if __name__ == "__main__":
     # create a map with all restaurant names as the key and
     # a minHeap of its menu items as the value
     restaurants = {}
+    restaurant_index = {}
     df = pd.read_excel(dataset_name)
     rest_name = ""
     #iterate through excel sheet to add restaurants
     for index, row in df.iterrows():
-        # if restaurant not already added
-        if(rest_name != restaurants[row['Restaurant']]):
+        # if restaurant already added, add that item to its heap
+        if row['Restaurant'] in restaurants:
+            # INSERT ITEM TO EXISTING MINHEAP AT THIS INDEX
+
+        # if not already in set, add the restaurant and set value to none
+        else:
             rest_name = row['Restaurant']
-            minheap = MinHeap.MinHeap(restaurants) # create a new heap to add as the value
-            restaurants[rest_name] = minheap
+            # INSERT ITEM TO MINHEAP AT INDEX rest_name
