396bd59d0d502df8812c4d025dfd93b21894512c
tmei005 <tmei@ufl.edu>
2024-04-21 17:58:08 -0400

    restaurant categories display

--- backend/static/cat.js
+++ backend/static/cat.js
@@ -1,14 +1,9 @@
-// List of image URLs
-console.log(catImages)
-// Initialize index to 0
 let index = 0;
-
 // Function to change image
 function changeImage() {
     // Update src attribute with the next image URL
     document.getElementById("catAnimation").src = catImages[index];
     index = (index + 1) % catImages.length;
 }
-
-// Call changeImage function every second
+// Call changeImage function every 800ms
 setInterval(changeImage, 800);
