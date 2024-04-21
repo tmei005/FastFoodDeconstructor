694e489c1650a2fa6a0b20a638a50973ccf46f94
tmei005 <tmei@ufl.edu>
2024-04-21 05:30:11 -0400

    getting input

--- backend/server.py
+++ backend/server.py
@@ -12,24 +12,24 @@ app.secret_key = 'user1'
 
 restMap = RestaurantMap.RestaurantMap()
 selected_restaurant = ''
-@app.route('/process_input', methods=['POST'])
-def process_input():
-    user_input = request.form['restaurant']
-    # Now you can use the user_input in your Python code
-    session['user1'] = user_input
-    # Process the input further as needed
-    return user_input
+# @app.route('/process_input', methods=['POST'])
+# def process_input():
+#     user_input = request.form['restaurant']
+#     # Now you can use the user_input in your Python code
+#     session['user1'] = user_input
+#     # Process the input further as needed
+#     return user_input
 @app.route('/')
 def page1():
     restaurants = list(restMap.restaurant_categories.keys())
     restaurants = restaurants[:-1] # take out 'Nan'
-    print(selected_restaurant)
+    # print(selected_restaurant)
     return render_template('page1.html', restaurants_list=restaurants)
 
 @app.route('/page2.html')
 def page2():
-    categories = restMap.restaurant_categories[session.get('user1')].values()
-    print(categories)
+    #categories = restMap.restaurant_categories[session.get('user1')].values()
+    #print(categories)
     return render_template('page2.html')
 
 @app.route('/page3.html')
